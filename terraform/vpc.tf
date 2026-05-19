20206fbf18deb8cb3a6443de6440356e82006f10
jakkalilokesh <jakkalilokesh@gmail.com>
2026-05-19 14:25:23 +0530

    fix: add missing public route table association for subnet public_2 to enable worker connectivity

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -46,3 +46,9 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
